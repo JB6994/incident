2a17b06cd4f291465317c2ae3dd03678f6babb3f
Belaizi <cbelaizi@laposte.net>
2018-02-26 11:27:04 +0100

    ajout service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,15 @@ import { HeaderComponent } from './include/header/header.component';
 import { FooterComponent } from './include/footer/footer.component';
 import { MenuComponent } from './data/menu/menu.component';
 import { IncidentlistComponent } from './data/content/incidentlist/incidentlist.component';
+import {IncidentService} from './service/incident.service';
+import {Routes} from '@angular/router';
+import { HomeComponent } from './data/content/home/home.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'incidents', component: IncidentlistComponent}
+];
 
 @NgModule({
   declarations: [
@@ -15,12 +23,13 @@ import { IncidentlistComponent } from './data/content/incidentlist/incidentlist.
     HeaderComponent,
     FooterComponent,
     MenuComponent,
-    IncidentlistComponent
+    IncidentlistComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [IncidentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
