688fcea93c90f49a26826b8424f94419571fde5f
julien bertrand <julien.b.bertrand@outlook.com>
2018-02-26 11:31:36 +0100

    add incident service implementation

--- src/app/service/incident.service.ts
+++ src/app/service/incident.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class IncidentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  list(): Observable<any> {
+    return this.http.get('http://localhost:8080/incident/incidents');
+  }
+
+  private handleError(error: any) {
+    const errMsg = (error.message) ? error.message : (error.status ? `${error.status} - ${error.statusText}` : 'Server error');
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+}
 }
